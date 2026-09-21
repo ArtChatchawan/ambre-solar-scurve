86562f3d5f3366904c1892d5199703fa73b59ff3
Chatchawan Sarnthong <artcivil@gmail.com>
2026-09-21 10:34:50 +0700

    Add files via upload

---
+++ Update_Dashboard.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ========================================================
+echo   Updating Interactive S-Curve Dashboard from Excel...
+echo ========================================================
+python update_dashboard.py
+echo.
+echo Sync Complete! Please refresh your browser page (F5).
+echo ========================================================
+pause
